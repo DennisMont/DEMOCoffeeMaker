588bc5376b3608ad7b605d8bd94e547ab20da717
melvicaballero <melvi.caballero@gmail.com>
2018-09-09 22:41:11 -0400

    added unit tests

--- BoilerAguaTest.java
+++ BoilerAguaTest.java
@@ -6,11 +6,38 @@ import org.junit.Test;
 public class BoilerAguaTest {
 
 	@Test
-	public void test() {
+	public void testStartBoiler() {
 		BoilerAgua ba = new BoilerAgua();
-		
 		ba.turnOnHeater();
 		assertEquals(ba.getHeaterState(), EnumDeviceState.on);
+	}
+	
+	@Test
+	public void testShutdownBoiler() {
+		BoilerAgua ba = new BoilerAgua();
+		ba.turnOnHeater();
+		ba.turnOffHeater();
+		assertEquals(ba.getHeaterState(), EnumDeviceState.off);
+	}
+	
+	@Test
+	public void testBoilerEmptyByDefect() {
+		BoilerAgua ba = new BoilerAgua();
+		assertTrue(ba.isEmpty());
+	}
+	
+	@Test
+	public void testOpenValve() {
+		BoilerAgua ba = new BoilerAgua();
+		ba.OpenValve();
+		assertTrue(ba.IsValveOpen());
+	}
+	
+	@Test
+	public void testCloseValve() {
+		BoilerAgua ba = new BoilerAgua();
+		ba.CloseValve();
+		assertFalse(ba.IsValveOpen());
 	}
 
 }
--- WaterSensor.java
+++ WaterSensor.java
@@ -1,6 +1,10 @@
 package cfd;
 public class WaterSensor extends DigitalSensor{
 
+	
+	public WaterSensor() {
+		state = EnumBoilerState.boilerEmpty;
+	}
 	public void setState(ITypeEnum typ) {
 		
 		if ( typ==EnumBoilerState.boilerEmpty)
